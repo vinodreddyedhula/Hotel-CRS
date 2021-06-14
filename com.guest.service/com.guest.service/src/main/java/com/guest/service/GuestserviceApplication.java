25ad7d8fb93cd1cb937876f97b27aade8e532913
Vinod Reddy <Vinodkumar_Eddula@epam.com>
2021-06-14 15:29:47 +0530

    Added GuestMS

---
+++ com.guest.service/com.guest.service/src/main/java/com/guest/service/GuestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.guest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestserviceApplication.class, args);
+	}
+
+}
